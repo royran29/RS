ed69021a70c1620c63dc576fcd07d35e71e806f3
Royran Castro Padilla <royran29@gmail.com>
2018-05-11 14:19:27 -0600

    Complete 2 pages

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Copa Subway</title>
+        <title>Subway</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
--- premio.php
+++ premio.php
@@ -16,7 +16,7 @@
     <meta property="og:image" content="http://copasubway.com/img/General/fb.jpg"/>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Copa Subway</title>
+    <title>Subway</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -39,49 +39,73 @@
         <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
     <![endif]-->
 
-        <!-- Content -->
-        <div class="container">
-            <div>
-                <div class="form-header">
-                    <img src="img/General/Fondos/1.png" alt="Fondo Formulario" >
-                    <div class="logos">
-                        <img src="img/General/Logos/subway_logo_verde.png" alt="Logo Copa">
-                    </div>
-                </div>
-                <div class="form-title">
-                    <p class="green-text sixInch">Completá este formulario para saber si ganaste unos de nuestros premios.</p>
-                </div>
+    <?php
+       
+        $database = new Database();
+        $db = $database->getConnection();
+        $premio = new Premio($db);
+        $premio->factura     = filter_var(trim($_POST["factura"]),     FILTER_SANITIZE_STRING);
+        $premio->total       = filter_var(trim($_POST["total"]),       FILTER_SANITIZE_STRING);
+        //$premio->restaurante = filter_var(trim($_POST["restaurante"]), FILTER_SANITIZE_STRING);
+        $premio->fecha       = filter_var(trim($_POST["fecha"]),       FILTER_SANITIZE_STRING);
+        $premio->validarPremio();
+        
+        //echo $premio->estado.'<br>';
 
-                <?php
-                
-                    $database = new Database();
-                    $db = $database->getConnection();
-                    $premio = new Premio($db);
-                    $premio->factura     = filter_var(trim($_POST["factura"]),     FILTER_SANITIZE_STRING);
-                    $premio->total       = filter_var(trim($_POST["total"]),       FILTER_SANITIZE_STRING);
-                    //$premio->restaurante = filter_var(trim($_POST["restaurante"]), FILTER_SANITIZE_STRING);
-                    $premio->fecha       = filter_var(trim($_POST["fecha"]),       FILTER_SANITIZE_STRING);
-                    $premio->validarPremio();
-                    
-                    echo $premio->estado.'<br>';
+        //echo $premio->mensaje.'<br>';
+        //valores de estado
+        // "valido", "sin premio", "cambiado", "raspado", "expirado"
+        // para cada uno de ellos hay que generar una vista que esta en la pagina 2 del PDF que te envie
 
-                    echo $premio->mensaje.'<br>';
-                    //valores de estado
-                    // "valido", "sin premio", "cambiado", "raspado", "expirado"
-                    // para cada uno de ellos hay que generar una vista que esta en la pagina 2 del PDF que te envie
-                    
-                ?>
-                <p class="cr sixInch">Imágenes con fines ilustrativos. Subway® es una marca registrada de Subway IP Inc. @2018 Subway IP Inc.</p>            
+        $titulo = "";
+        $verRaspa = false;
+
+        switch($premio->estado){
+            case "valido":
+                $titulo = "¡Felicidades!";
+                $verRaspa = true;
+                break;
+            case "sin premio":
+                $titulo = "¡Comprá tu combo!";
+                break;
+            case "raspado":
+                break;
+            case "expirado":
+                $titulo = "¡Participá de nuevo!";
+                break;
+            default:
+                $titulo = "¡Itenta mas tarde!";
+        }
+        
+    ?>
+
+
+    <!-- Content -->
+    <div class="container green-background-info">
+        <div class="banderines">
+            <img src="img/Raspas/Banderines.png" alt="Banderines">
+        </div>
+        <div class="content premio"> 
+            <h2 class="yellow-text footlong"><?php echo $titulo; ?></h2>
+            <p class="white-text footlong"><?php echo $premio->mensaje;?></p>
+            <?php if(!$verRaspa){ ?>
+                <a href="#"><img id="imgReglamento" src="img/Raspas/Botones/Ver reglamento.png" alt="Ver reglamento"></a>
+                <a href="index.php"><img id="imgVolver" src="img/Raspas/Botones/Volver.png" alt="Volver"></a>
+            <?php } else{?>
+                <a href="#"><img id="imgRaspa"  src="img/Raspas/Botones/Raspar.png" alt="Raspar"></a>
+            <?php }//cierre if else ?>
+            
+            <div class="logos">
+                <img src="img/Raspas/S Subway.png" alt="S Subway">
+                <img src="img/Raspas/Coca Cola.png" alt="Coca Cola">
             </div>
         </div>
+    </div>
         
-        <script src="js/vendor/modernizr-3.5.0.min.js"></script>
-        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
-
-        
-
+    <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
     </body>
 </html>
